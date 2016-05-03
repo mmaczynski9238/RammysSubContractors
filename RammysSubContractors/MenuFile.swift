b3f360d8a4ed5816b0efdf9b4f9b5e35f55f0324
student1 <student1@jh-mbair-158-03.d214.org>
2016-05-03 14:42:13 -0500

    Menu Class

---
+++ RammysSubContractors/MenuFile.swift
@@ -0,0 +1,63 @@
+//
+//  MenuFile.swift
+//  RammysSubContractors
+//
+//  Created by student1 on 5/3/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import Foundation
+
+class Menu {
+    var turkey = String()
+    var Roast = String()
+    var hammer = String()
+    var trilevel = String()
+    var reuben = String()
+    var italainbeef = String()
+    var tuna = String()
+    var twobyfour = String()
+    var lien = String()
+    var wrecking = String()
+    var seafoodcrab = String()
+    var BBQ = String()
+    var radical = String()
+    var skyscraper = String()
+    var megastructure = String()
+    var BLT = String()
+    var sawhorse = String()
+    var ultimatesawhorse = String()
+    var classicchicken = String()
+    var BBQchicken = String()
+    var parmesan = String()
+
+
+    
+    
+    init() {
+        turkey = "Turkey & Swiss"
+        Roast = "Roast Beef & Cheese"
+        hammer = "The Hammer & Cheese"
+        trilevel = "The Tri-Level"
+        reuben = "Rammy's Reuben"
+        italainbeef = "Italian Beef with Provolone"
+        tuna = "Tuna & Provolone"
+        twobyfour = "The Two by Four"
+        lien = "The Lien"
+        wrecking = "The Wrecking Ball"
+        seafoodcrab = "Seafood & Crab with Provolone"
+        BBQ = "Rammy's BBQ"
+        radical = "Rammy's Radical Italian Beef"
+        skyscraper = "Skyscraper"
+        megastructure = "Megastructure"
+        BLT = "BLT"
+        sawhorse = "Sawhorse"
+        ultimatesawhorse = "Ultimate Sawhorse"
+        classicchicken = "Classic Chicken"
+        BBQchicken = "BBQ Chicken"
+        parmesan = "Parmesan Chicken"
+        
+        
+    }
+    
+}
