7c8cc30656fcf628b2f9a7155ec5cbcd9e421568
student3 <student3@jh-mbair-158-10.d214.org>
2016-05-05 14:22:03 -0500

    Added outlet to map view

--- RammysSubContractors/AboutViewController.swift
+++ RammysSubContractors/AboutViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class AboutViewController: UIViewController, ENSideMenuDelegate {
     
     @IBOutlet var aboutButtonMainVC: UIBarButtonItem!
     
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
